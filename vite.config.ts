fd50ad74c44e9f7fccfbc0e010040faf91dc26be
Sam <centersam19@gmail.com>
2024-10-08 17:57:41 -0400

    Existing Project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/main.ts',
+      name: 'LivePlayer',
+      fileName: 'live-player'
+    }
+  },
+  plugins: [svelte({
+    include: ['src/VideoPlayer.svelte'],
+    compilerOptions: {
+      css: true,
+      customElement: true
+    }
+  })]
+})
